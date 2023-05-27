85cd44d3b1149559717ffd9f1643ddcbecf98b08
Eduardo Piaia <65435954+EPiaia@users.noreply.github.com>
2023-05-26 21:03:38 -0300

    Configuração do banco de dados

--- pi5/pi5/settings.py
+++ pi5/pi5/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pi5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projeto5',
+        'USER': 'postgres',
+        'PASSWORD': '150623',
+        'HOST': 'localhost'
+
     }
 }
 
